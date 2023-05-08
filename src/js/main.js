ddb86b44f707efc77976bd6f809cddc41118ace3
Colanatorsos <alibekmah527@gmail.com>
2023-05-09 00:17:01 +0600

    Добавил main переходник

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// Добавляем обработчики событий для нажатия на карточку
+const cards = document.querySelectorAll('.card');
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    // Действие при нажатии на карточку
+  })
+});
